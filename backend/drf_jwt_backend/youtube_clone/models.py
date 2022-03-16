8b96644c132a74e8f7ba3789577b948594163eb0
Sydney Wilson <syd.toon17@gmail.com>
2022-03-16 16:51:26 -0500

    Sydney: started workin on setting up serializers and modeles. My partner was out sick today.

---
+++ backend/drf_jwt_backend/youtube_clone/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_id = models.CharField(max_length= 255)
+    text = models.CharField(max_length= 255)
+    likes = models.IntegerField()
+    dislikes = models.IntegerField()
+
+class Reply(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    text = models.CharField(max_length= 255)
