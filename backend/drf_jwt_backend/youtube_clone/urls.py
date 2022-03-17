b0654e8c2981531b33869ec2baca2f99c7440627
Sydney Wilson <syd.toon17@gmail.com>
2022-03-17 14:28:49 -0500

    fixed the functionality of urls so get request works now.

--- backend/drf_jwt_backend/youtube_clone/urls.py
+++ backend/drf_jwt_backend/youtube_clone/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from youtube_clone import views
 
 urlpatterns = [
-    path('', views.user_comments),
-    # path('all/', views.get_all_comments),
-    path('<int:pk>/', views.get_all_comments),
+    path('', views.get_all_comments),
+    path('<int:pk>/', views.user_comments),
 ]
